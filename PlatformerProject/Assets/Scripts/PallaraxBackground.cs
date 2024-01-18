aee682770b61b53ed017fa965fb2e1d0a8bae19f
Hoàng Quang Vinh <124579934+VinhVIT@users.noreply.github.com>
2024-01-18 18:18:26 +0700

    Init

---
+++ PlatformerProject/Assets/Scripts/PallaraxBackground.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PallaraxBackground : MonoBehaviour
+{   
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    private Transform cameraTrans;
+    private Vector3 lastCamPos;
+    private void Start() {
+        cameraTrans = Camera.main.transform;
+        lastCamPos = cameraTrans.position;
+    }
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTrans.position - lastCamPos;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x,deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCamPos = cameraTrans.position;
+    }
+}
